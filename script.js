8ad170a4ca995ef5495666b0b733eb595f29e9e7
Denis Fernando <denisferoli@gmail.com>
2023-03-17 21:12:07 -0300

    update

---
+++ script.js
@@ -0,0 +1,17 @@
+
+
+//function saudacao(){
+    var data = new Date();
+    var hora = data.getHours();
+    var saudacao = document.createElement("h2");
+    var pai = document.querySelector("article");
+    if(hora >= 1 && hora < 12){
+        saudacao.textContent = "Olá, Bom dia!";
+    } else if(hora >= 12 && hora < 18){
+        saudacao.textContent = "Olá, Boa tarde!"
+    } else {
+        saudacao.textContent = "Olá, Boa Noite!"
+    }
+
+    pai.insertBefore(saudacao, bem);
+//}
